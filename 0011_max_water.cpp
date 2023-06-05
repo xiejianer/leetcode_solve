e5aa9ea993d7e6ff41a0d2cd93db0659c9ce4dec
xiejianer <xiejianer@xiejianer.com>
2023-06-05 22:31:24 +0800

    finish 11, think more

---
+++ 0011_max_water.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i = 0;
+        int j = height.size() - 1;
+        int res = 0;
+        while (i < j) {
+            if (height[i] < height[j]) {
+                res = max(res, height[i] * (j - i));
+                ++i;
+            } else {
+                res = max(res, height[j] * (j - i));
+                --j;
+            }
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    return 0;
+}
